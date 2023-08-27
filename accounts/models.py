f2c336664fb3932926086051ba4fc2b52e19c6be
ibrahimfqaisi <qasrawi.ibrahim1998@gmail.com>
2023-08-27 15:08:02 +0300

    add model

---
+++ accounts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    USER_TYPE_CHOICES = (
+        ('customer', 'Customer'),
+        ('technician', 'Technician'),
+    )
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default='customer')
+
+    # def is_customer(self):
+    #     return hasattr(self, 'customerprofile')
+
+    # def is_technician(self):
+    #     return hasattr(self, 'technicianprofile')
---
+++ technician/models.py
@@ -0,0 +1,18 @@
+# technicians/models.py
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from accounts.models import CustomUser
+
+class TechnicianProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    profession = models.CharField(max_length=255)
+    rating = models.IntegerField()
+    feedback = models.TextField()
+    image = models.CharField(max_length=255)
+    description = models.TextField()
+
+@receiver(post_save, sender=CustomUser)
+def create_technician_profile(sender, instance, created, **kwargs):
+    if created and instance.user_type == 'technician':
+        TechnicianProfile.objects.create(user=instance)
