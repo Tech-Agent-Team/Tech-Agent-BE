c167e14a6f82e2993057cd7f05149b92cdef1dd5
BayanBanat <bayankbanat99@gmail.com>
2023-08-28 09:19:44 +0000

    version-1-bayan

--- technician/serializers.py
+++ technician/serializers.py
@@ -2,27 +2,70 @@ from rest_framework import serializers
 from .models import TechnicianProfile
 from accounts.models import CustomUser
 
+# class TechnicianProfileSignUpSerializer(serializers.ModelSerializer):
+#     password2=serializers.CharField(style={"input_type": "password"},write_only=True)
+#     class Meta:
+#         model= CustomUser
+#         fields = ['username','email', 'password', 'password2']
+#         extra_kwargs={
+#             'password': {'write_only': True}
+#         }
+#     def save(self, **kwargs):
+        
+#         user=CustomUser(
+#             username=self.validated_data['username'],
+#             email=self.validated_data['email']
+#         )
+#         password=self.validated_data['password']
+#         password2=self.validated_data['password2']
+#         if password !=password2:
+#             raise serializers.ValidationError({"error":"password do not match"})
+#         user.set_password(password)
+#         user.is_technician=True
+#         user.save()
+#         TechnicianProfile.objects.create(user=user)
+#         return user
+    
+
 class TechnicianProfileSignUpSerializer(serializers.ModelSerializer):
-    password2=serializers.CharField(style={"input_type": "password"},write_only=True)
+    password2 = serializers.CharField(style={"input_type": "password"}, write_only=True)
+    profession = serializers.CharField(max_length=255)
+    image = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+
     class Meta:
-        model= CustomUser
-        fields = ['username','email', 'password', 'password2']
-        extra_kwargs={
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'password2', 'profession', 'image', 'description']
+        extra_kwargs = {
             'password': {'write_only': True}
         }
+
     def save(self, **kwargs):
-        
-        user=CustomUser(
+        user = CustomUser(
             username=self.validated_data['username'],
             email=self.validated_data['email']
         )
-        password=self.validated_data['password']
-        password2=self.validated_data['password2']
-        if password !=password2:
-            raise serializers.ValidationError({"error":"password do not match"})
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+        if password != password2:
+            raise serializers.ValidationError({"error": "password do not match"})
         user.set_password(password)
-        user.is_technician=True
+        user.is_technician = True
         user.save()
-        TechnicianProfile.objects.create(user=user)
+
+        TechnicianProfile.objects.create(
+            user=user,
+            profession=self.validated_data['profession'],
+            image=self.validated_data['image'],
+            description=self.validated_data['description']
+        )
         return user
-    
+# {
+#   "username":"bayan",
+#   "email":"bayan@gmail.com",
+#   "password":"1234",
+#   "password2":"1234",
+#   "profession":"eng",
+#   "image":"dd",
+#   "description":"lolo"
+# }
