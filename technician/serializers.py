1d83ad45294ce9fc03c1fe6c3f45ab4e6d6d5eb8
ibrahimfqaisi <qasrawi.ibrahim1998@gmail.com>
2023-09-04 15:50:25 +0300

    i fixed regester form

--- technician/serializers.py
+++ technician/serializers.py
@@ -1,19 +1,19 @@
 from rest_framework import serializers
 from .models import TechnicianProfile , Profession
-
 from orders.models import Order ,Comment
 from accounts.models import CustomUser
 from django.contrib.auth import get_user_model
 from accounts.serializers import CustomUserSerializer,CustomUserWithNoPassword
+from rest_framework import serializers
 
 class TechnicianProfileSignUpSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(style={"input_type": "password"}, write_only=True)
     professions = serializers.ListField(child=serializers.CharField(max_length=255))
-    image = serializers.CharField(max_length=255)
+    image = serializers.ImageField()  # Use ImageField for image uploads
     description = serializers.CharField()
 
     class Meta:
-        model = CustomUser
+        model = get_user_model()
         fields = ['username', 'email', 'password', 'password2', 'image', 'description', 'professions']
         extra_kwargs = {
             'password': {'write_only': True}
@@ -25,33 +25,41 @@ class TechnicianProfileSignUpSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("This email address is already registered.")
         return value
     
-    def save(self, **kwargs):
-        user = CustomUser(
-            username=self.validated_data['username'],
-            email=self.validated_data['email']
-        )
-        password = self.validated_data['password']
-        password2 = self.validated_data['password2']
+    def create(self, validated_data):
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data['password']
+        password2 = validated_data['password2']
+        image = validated_data['image']
+        description = validated_data['description']
+        professions_string = validated_data.get('professions', [])
+        professions_list = professions_string[0].split(',')
+
+
         if password != password2:
-            raise serializers.ValidationError({"error": "password do not match"})
-        user.set_password(password)
-        user.is_technician = True
-        user.save()
+            raise serializers.ValidationError({"error": "Passwords do not match"})
+
+        user = get_user_model().objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+            is_technician=True
+        )
 
         technician_profile = TechnicianProfile.objects.create(
             user=user,
-            image=self.validated_data['image'],
-            description=self.validated_data['description']
+            image=image,
+            description=description
         )
 
-        professions = self.validated_data.get('professions', [])
-        for profession in professions:
+        for profession in professions_list:
             Profession.objects.create(
                 technicianProfession=technician_profile,
                 techProfession=profession
             )
 
         return user
+
 
 class homeTechnicianSerializers(serializers.ModelSerializer):
     class Meta:
@@ -96,6 +104,12 @@ class TechnicianUpdateProfileSerializer(serializers.ModelSerializer):
         model = TechnicianProfile
         fields = ['professions', 'image', 'description']
 
+    def validate_email(self, value):
+        user_model = get_user_model()
+        if user_model.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email address is already registered.")
+        return value
+    
     def to_representation(self, instance):
         data = super().to_representation(instance)
         data['professions'] = [profession.techProfession for profession in instance.professions.all()]
